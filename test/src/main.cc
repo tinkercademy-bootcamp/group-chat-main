2b376385ca2bc5f03e3ddf09e621dfc07fbe0238
Rajasree Ponnada <user@ip-172-31-25-255.ap-south-1.compute.internal>
2025-06-07 03:39:29 +0000

    feat: place the main loop to test

---
+++ test/src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test-client.h"
+
+int main(int argc, char* argv[]) {
+    (void)argc; (void)argv; 
+    std::cout << "Test Framework Main - To be implemented" << std::endl;
+    return 0;
+}
+  
