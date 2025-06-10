f853e67dd858ee4b7ba0425712618b92babc9a86
Balaji <balajikaredla4@gmail.com>
2025-06-10 12:10:32 +0530

    add utils.h that has rdtsc

---
+++ udp-test/utils.h
@@ -0,0 +1,8 @@
+unsigned long long rdtsc() {
+    unsigned long long a, d;
+    asm volatile("mfence");
+    asm volatile("rdtsc" : "=a"(a), "=d"(d));
+    a = (d << 32) | a;
+    asm volatile("mfence");
+    return a;
+}
