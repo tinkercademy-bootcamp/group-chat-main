a6eb75d6895f53056da81b5f6a0cc7e290798fa7
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-06-11 11:32:45 +0000

    perf- optimize for performance by making some changes

--- auto_profiler.sh
+++ auto_profiler.sh
@@ -6,18 +6,18 @@
 BIN=./build/server
 DURATION=${2:-10}   # seconds
 
-sudo perf record -F 997 -g -- "$BIN" & # For reasons that'll take a while to explain, do NOT make it a multiple of 10.
+sudo perf record -F 197 -g -- "$BIN" & # For reasons that'll take a while to explain, do NOT make it a multiple of 10.
 PID=$!
 echo "Server PID=$PID. Profiling for $DURATION s…"
 
-sleep 4 # sleep long enough to start the server
+sleep 1 # sleep long enough to start the server
 if [[ "$*" == *"--auto"* ]]; then
-    ./test/chat_load_tester 127.0.0.1 8080 100 100 64 1 10 testchannel
+    ./test/chat_load_tester 127.0.0.1 8080 10 1000 64 1 10 testchannel
     # Usage: ./test/chat_load_tester <server_ip> <server_port> <num_clients> <messages_per_client> <message_size_bytes> [listen_replies (0 or 1)] [think_time_ms (0+)] [channel_name]
     # Example: ./test/chat_load_tester 127.0.0.1 8080 10 100 64 1 10 testchannel
 fi
 
-sleep "$DURATION"
+sleep 1
 sudo kill -INT "$PID"   # graceful Ctrl-C
 
 mkdir -p ./profiling-data
